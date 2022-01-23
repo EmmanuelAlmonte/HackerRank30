856b8a942db0e9d1503d8e5f07e3935a4c6e6c68
EmmanuelAlmonte <ammetronics67@gmail.com>
2022-01-23 07:48:39 -0500

    adding day 1 and 2 solutions

---
+++ dayOne.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank' 
+
+# Read and save an integer, double, and String to your variables.
+secInt = int(input("Enter an Integer: "))
+secDouble = float(input("Input an double: "))
+secString = str(input("Input a String: "))
+
+# Print the sum of both integer variables on a new line.
+print(secInt + i)
+# Print the sum of the double variables on a new line.
+print(secDouble + d)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + "" + secString)
+ 
