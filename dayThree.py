2b00cdf92046b5ad9442382e0a05c0a96cd77850
EmmanuelAlmonte <ammetronics67@gmail.com>
2022-01-25 20:22:58 -0500

    day Three added.

---
+++ dayThree.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def nOddOrEven(inputNum):
+  if(inputNum % 2 != 0):
+    print("Weird")
+  elif(inputNum % 2 == 0) and (inputNum in range(2, 6)):
+    print("Not Weird")
+  elif(inputNum % 2 == 0) and (inputNum in range(6, 21)):
+    print("Weird")
+  elif(inputNum > 20):
+    print("Not Weird")
+  
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    nOddOrEven(N)
+
