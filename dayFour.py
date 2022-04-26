e70e1563d856b20d3f0addda78fe5d387bcfec2d
EmmanuelAlmonte <emman9222@gmail.com>
2022-04-26 15:27:05 -0400

    Completed Day Fourin Javascript

--- dayFour.py
+++ dayFour.py
@@ -1,10 +1,16 @@
+from mimetypes import init
+import this
+
+
 class Person:
     def __init__(self,initialAge):
         # Add some more code to run some checks on initialAge
+        this.age = initialAge
     def amIOld(self):
         # Do some computations in here and print out the correct statement to the console
     def yearPasses(self):
         # Increment the age of the person in here
+        
 
 t = int(input())
 for i in range(0, t):
